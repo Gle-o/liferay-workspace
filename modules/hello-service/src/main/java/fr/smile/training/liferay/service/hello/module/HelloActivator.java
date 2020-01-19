85e0268518838cbf7de3b2654c5357a5c4408741
Guillaume Lenoir <guillaume.lenoir@ext.leroymerlin.fr>
2020-01-19 13:27:08 +0100

    Add activator

---
+++ modules/hello-service/src/main/java/fr/smile/training/liferay/service/hello/module/HelloActivator.java
@@ -0,0 +1,19 @@
+package fr.smile.training.liferay.service.hello.module;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * @author guillaumelenoir
+ */
+public class HelloActivator implements BundleActivator {
+
+	@Override
+	public void start(BundleContext bundleContext) throws Exception {
+	}
+
+	@Override
+	public void stop(BundleContext bundleContext) throws Exception {
+	}
+
+}
