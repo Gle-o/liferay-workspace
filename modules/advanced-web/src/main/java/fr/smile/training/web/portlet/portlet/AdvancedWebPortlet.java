85a91bda95e137366a98db5391905d4b65ab5d9e
Guillaume Lenoir <guillaume.lenoir@ext.leroymerlin.fr>
2020-01-29 13:47:44 +0100

    init portlet

---
+++ modules/advanced-web/src/main/java/fr/smile/training/web/portlet/portlet/AdvancedWebPortlet.java
@@ -0,0 +1,30 @@
+package fr.smile.training.web.portlet.portlet;
+
+import fr.smile.training.web.portlet.constants.AdvancedWebPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author guillaumelenoir
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=AdvancedWeb",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + AdvancedWebPortletKeys.ADVANCEDWEB,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class AdvancedWebPortlet extends MVCPortlet {
+}
