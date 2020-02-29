0a78b52f6b5456db84e0156ee992da71cd633bbb
Guillaume Lenoir <guillaume.lenoir@ext.leroymerlin.fr>
2020-02-29 15:49:52 +0100

    add portlet fat and service
    add portlet fat and service

--- modules/announcement/announcement-service/src/main/resources/META-INF/sql/indexes.sql
+++
@@ -1,4 +0,0 @@
-create index IX_901A23A7 on FOO_Faq (groupId);
-create index IX_E810FA81 on FOO_Faq (status, groupId);
-create index IX_70DE5F97 on FOO_Faq (uuid_[$COLUMN_LENGTH:75$], companyId);
-create unique index IX_80D7DFD9 on FOO_Faq (uuid_[$COLUMN_LENGTH:75$], groupId);
---
+++ modules/faq/faq-web/src/main/java/fr/smile/training/faq/web/portlet/FaqPortlet.java
@@ -0,0 +1,30 @@
+package fr.smile.training.faq.web.portlet;
+
+import fr.smile.training.faq.web.constants.FaqPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author guillaumelenoir
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=Faq",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + FaqPortletKeys.FAQ,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class FaqPortlet extends MVCPortlet {
+}
