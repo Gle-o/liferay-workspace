220f6412b6165871639eb7e1102f6c90487bcad5
Guillaume Lenoir <guillaume.lenoir@ext.leroymerlin.fr>
2020-03-02 21:25:53 +0100

    add @Deactivate and @Activate
    add @Deactivate and @Activate

--- modules/hello-service/src/main/java/fr/smile/training/liferay/service/api/impl/HelloServiceImpl.java
+++ modules/hello-service/src/main/java/fr/smile/training/liferay/service/api/impl/HelloServiceImpl.java
@@ -1,13 +1,29 @@
 package fr.smile.training.liferay.service.api.impl;
 
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 
 import fr.smile.training.liferay.service.api.api.HelloService;
+import org.osgi.service.component.annotations.Deactivate;
 
-@Component(immediate = true, property = { "foo=bar", "foo=baz"
-
-}, service = HelloService.class)
+@Component(immediate = true,
+		service = HelloService.class)
 public class HelloServiceImpl implements HelloService {
+
+	private static final Log _log = LogFactoryUtil.getLog(
+			HelloServiceImpl.class);
+	@Activate
+	public void start(BundleContext bundleContext) throws Exception {
+		_log.info("########### Service Registered Successfully ##############");
+	}
+
+	@Deactivate
+	public void stop(BundleContext bundleContext) throws Exception {
+		_log.info("########### Service Unregistered Successfully ##############");
+	}
 
 	@Override
 	public String sayHello(String firstName) {
